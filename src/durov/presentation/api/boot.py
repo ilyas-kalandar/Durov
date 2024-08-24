b12ecd895498ad73507e5d684725db7735d93366
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2024-08-24 18:56:53 +0500

    Fix: Don't creating engine, accepting `engine` as dependency.

--- src/durov/presentation/api/boot.py
+++ src/durov/presentation/api/boot.py
@@ -15,6 +15,7 @@ def init_logging(conf: Settings):
     logging.basicConfig(
         level=conf.logging_level, format=conf.logging_format, force=True
     )
+    logging.info("Logging configured.")
 
 
 async def get_engine(conf: Settings) -> AsyncEngine:
@@ -34,12 +35,11 @@ async def get_engine(conf: Settings) -> AsyncEngine:
     return engine
 
 
-async def get_session_maker(conf: Settings) -> async_sessionmaker:
+async def get_session_maker(engine: AsyncEngine) -> async_sessionmaker:
     """
     Builds & returns session-maker
     :return: Initialized session-maker
     """
-    engine = await get_engine(conf)
     return async_sessionmaker(engine, autoflush=True, expire_on_commit=False)
 
 
@@ -49,8 +49,3 @@ def get_uvicorn_logging_conf(conf: Settings):
     log_config["formatters"]["default"]["fmt"] = conf.logging_format
 
     return log_config
-
-
-async def destroy_engine(engine: AsyncEngine):
-    """Destroys engine"""
-    await engine.dispose()
