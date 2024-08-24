7d0f2f508493769c7d9bd0bb7771325763cafb0b
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2024-08-24 17:20:44 +0500

    Feature: Implemented core-logic of task.
    Now, using `asyncio.Future` for creating something like queue.

--- src/durov/infra/adapters/repos/user.py
+++ src/durov/infra/adapters/repos/user.py
@@ -1,3 +1,8 @@
+import asyncio
+from collections import defaultdict
+from asyncio import Future
+from typing import Dict
+
 from sqlalchemy import select
 
 from durov.app.dto.user import UserCreationParamsDTO
@@ -6,6 +11,16 @@ from durov.app.models.user import User
 from durov.app.repos.user import UserRepoInterface
 from durov.infra.adapters.repos.base import BaseSQLAlchemyRepo
 from durov.infra.db import models
+
+# Mock-cache
+CACHE: Dict[str, User] = {}
+
+# For futures
+FUTURES: Dict[str, Future[User]] = {}
+REFS: Dict[str, int] = defaultdict(int)
+
+# Locker
+lock = asyncio.Lock()
 
 
 class UserRepo(BaseSQLAlchemyRepo, UserRepoInterface):
@@ -33,12 +48,10 @@ class UserRepo(BaseSQLAlchemyRepo, UserRepoInterface):
 
         return obj.to_domain()
 
-    async def get_by_nickname(self, nickname: str) -> User:
-        """
-        Returns user by its nickname
-        :param nickname: A nickname (ex: durov)
-        :return: `User` object if found
-        """
+    async def _get_from_db(self, nickname: str) -> User:
+        # Very slow method
+
+        await asyncio.sleep(10)  # 10 seconds delay
 
         results = await self._session.execute(
             select(models.User).where(models.User.nickname == nickname).limit(1)
@@ -50,3 +63,40 @@ class UserRepo(BaseSQLAlchemyRepo, UserRepoInterface):
             raise UserNotFound("User not found")
 
         return first_one.to_domain()
+
+    async def _get_from_cache(self, nickname: str) -> User:
+        # Slow method too
+
+        await asyncio.sleep(3)
+
+        if nickname not in CACHE:
+            CACHE[nickname] = await self._get_from_db(nickname)
+
+        return CACHE[nickname]
+
+    async def get_by_nickname(self, nickname: str) -> User:
+        """
+        Returns user by its nickname
+        :param nickname: A nickname (ex: durov)
+        :return: `User` object if found
+        """
+
+        async with lock:
+            REFS[nickname] += 1
+
+            if nickname not in FUTURES:
+                loop = asyncio.get_event_loop()
+                FUTURES[nickname] = loop.create_future()
+                user = await self._get_from_cache(nickname)
+
+                FUTURES[nickname].set_result(user)
+
+        result = await FUTURES[nickname]
+
+        async with lock:
+            REFS[nickname] -= 1
+            if REFS[nickname] == 0:
+                del REFS[nickname]
+                del FUTURES[nickname]
+
+        return result
