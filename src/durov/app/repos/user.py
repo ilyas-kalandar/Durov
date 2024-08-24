cea3afea2ece59a86ff4b2f07c254551f1ff65ab
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2024-08-24 16:50:17 +0500

    Feature: Add `create` to `UserRepoInterface`

--- src/durov/app/repos/user.py
+++ src/durov/app/repos/user.py
@@ -1,10 +1,20 @@
 from abc import ABC, abstractmethod
 
+from durov.app.dto.user import UserCreationParamsDTO
 from durov.app.models.user import User
 
 
 class UserRepoInterface(ABC):
     """Contract for user-repo"""
+
+    @abstractmethod
+    async def create(self, params: UserCreationParamsDTO):
+        """
+        Creates a user
+        :param params: Creation params
+        :return: Created user
+        """
+        raise NotImplementedError
 
     @abstractmethod
     async def get_by_nickname(self, nickname: str) -> User:
